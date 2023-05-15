617ac6f7a4167f3a184244b07b959257d0ba9d6f
Vinayak Karigar <vinayak.karigar@grandvision.com>
2023-05-15 10:45:01 +0530

    Initial commit

---
+++ src/main/java/com/vin/learnjavapersistence/LearnJavaPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.vin.learnjavapersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaPersistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vin/learnjavapersistence/LearnJavaPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vin.learnjavapersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
